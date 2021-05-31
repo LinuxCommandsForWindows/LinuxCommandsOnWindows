8bba439d05fb25ecf1f95e6cc714e239f612080d
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-05-31 20:54:31 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,24 @@
-use winfetch::model::{
-    graphicscard,
-    hostsystem,
-    motherboard,
-    os,
-    processor,
-    processorusage,
-    screenres,
-    uptime,
-    winntkernel
+use winfetch::{
+    model::{
+        graphicscard,
+        hostsystem,
+        memory,
+        motherboard,
+        names,
+        os,
+        processor,
+        processorusage,
+        screenres,
+        storage,
+        uptime,
+        winntkernel,
+    },
+    utils
 };
 
 fn main() {
+    let names = names::Names::GetNames();
+
     let mut operating_system = os::OS::GetOperatingSystemVersion().unwrap();
     operating_system.GetOperatingSystemArchitecture().unwrap();
 
@@ -23,14 +31,26 @@ fn main() {
     let graphics_card = graphicscard::GraphicsCard::GetGraphicsCards().unwrap();
     let mut processor_usage = processorusage::ProcessorUsage::GetProcessorLoadPercentage().unwrap();
     processor_usage.GetProcessesCount().unwrap();
+    let memory = memory::Memory::GetMemoryStatistics().unwrap();
+    let drives = storage::Storage::GetStorageStatistics().unwrap();
 
-    println!("{}", operating_system);
-    println!("{}", host_system);
-    println!("{}", windows_nt_kernel);
-    println!("{}", motherboard);
-    println!("{}", uptime);
-    println!("{}", screen_resolutions);
-    println!("{}", processor);
-    println!("{}", graphics_card);
-    println!("{}", processor_usage);
+    let mut lines = utils::GetWindowsASCIIArt().lines().map(|refstr| refstr.to_string()).collect::<Vec<String>>();
+    lines[0].push_str(&format!("  {}", names));
+    lines[1].push_str(&format!("{}0m  {}", utils::ANSI_ESCAPE_SEQUENCE, String::from("-").repeat(
+        format!("{}@{}", names.UserName.clone().into_string().unwrap().to_ascii_lowercase(), names.ComputerName.clone().into_string().unwrap().to_ascii_lowercase()).len()).as_str()));
+    lines[2].push_str(&format!("  OS{}0m: {}", utils::ANSI_ESCAPE_SEQUENCE, operating_system));
+    lines[3].push_str(&format!("  Host System{}0m: {}", utils::ANSI_ESCAPE_SEQUENCE, host_system));
+    lines[4].push_str(&format!("  Kernel{}0m: {}", utils::ANSI_ESCAPE_SEQUENCE, windows_nt_kernel));
+    lines[5].push_str(&format!("  Motherboard{}0m: {}", utils::ANSI_ESCAPE_SEQUENCE, motherboard));
+    lines[6].push_str(&format!("  System Uptime{}0m: {}", utils::ANSI_ESCAPE_SEQUENCE, uptime));
+    lines[7].push_str(&format!("  Screen Resolutions(s){}0m: {}", utils::ANSI_ESCAPE_SEQUENCE, screen_resolutions));
+    lines[8].push_str(&format!("                                     Processor{}0m: {}", utils::ANSI_ESCAPE_SEQUENCE, processor));
+    lines[9].push_str(&format!("  Graphics Card(s){}0m: {}", utils::ANSI_ESCAPE_SEQUENCE, graphics_card));
+    lines[10].push_str(&format!("  Memory{}0m: {}", utils::ANSI_ESCAPE_SEQUENCE, memory));
+
+    println!();
+    lines.into_iter().for_each(|string| {
+        println!("{}", string)
+    });
+    println!();
 }
