1cfc7d1f0a8d13dfdcd99d200edb1c77b387e349
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-05-31 16:01:53 +0800

    Update mod.rs

--- src/model/mod.rs
+++ src/model/mod.rs
@@ -1,11 +1,11 @@
 pub mod graphicscard;
 pub mod hostsystem;
 pub mod memory;
-
 pub mod motherboard;
 pub mod os;
 pub mod processor;
 pub mod processorusage;
 pub mod screenres;
+pub mod storage;
 pub mod uptime;
 pub mod winntkernel;
