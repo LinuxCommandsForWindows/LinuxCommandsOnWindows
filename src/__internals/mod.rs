477faa9049eb6cb9fd2f808af58c44eb054f9700
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-06-01 13:39:01 +0800

    Delete src directory

--- src/__internals/mod.rs
+++
@@ -1,8 +0,0 @@
-pub fn __InternalsToUnits(value: f64) -> String {
-    return if value > 1024f64.powi(3) {
-        format!("{:.2} TB", value / 1024.0f64.powi(3))
-    }
-    else {
-        format!("{:.2} MB", value / 1024.0f64.powi(2))
-    }
-}
--- src/lib.rs
+++
@@ -1,6 +0,0 @@
-#![allow(non_snake_case)]
-
-mod __internals;
-pub mod error;
-pub mod model;
-pub mod utils;
