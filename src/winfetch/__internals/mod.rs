eb42e98dfa7ad46650c5a0392997b2907ddb4458
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-06-01 18:14:26 +0800

    Fix unit bug

--- src/winfetch/__internals/mod.rs
+++ src/winfetch/__internals/mod.rs
@@ -1,8 +1,8 @@
 pub fn __InternalsToUnits(value: f64) -> String {
-    return if value > 1024f64.powi(3) {
-        format!("{:.2} TB", value / 1024.0f64.powi(3))
+    return if value > 1024f64.powi(4) {
+        format!("{:.2} TB", value / 1024.0f64.powi(4))
     }
     else {
-        format!("{:.2} MB", value / 1024.0f64.powi(2))
+        format!("{:.2} GB", value / 1024.0f64.powi(3))
     }
 }
