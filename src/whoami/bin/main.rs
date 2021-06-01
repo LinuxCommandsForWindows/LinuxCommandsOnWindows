e5366b13da3732660882711e9e316a18d33754d5
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-06-01 14:36:32 +0800

    Update main.rs

--- src/whoami/bin/main.rs
+++ src/whoami/bin/main.rs
@@ -1,3 +1,26 @@
-fn main() {
+use std::env as environment;
 
+use linux_commands_on_windows::whoami;
+
+fn main() {
+    let mut commandline_arguments = environment::args();
+
+    if let Some(argument) = commandline_arguments.nth(1) {
+        match &*argument {
+            "--help" => {
+                println!("{}", whoami::WHOAMI_HELP_MESSAGE);
+                return;
+            },
+            "--version" => {
+                println!("{}", whoami::WHOAMI_VERSION_MESSAGE);
+                return;
+            }
+            _ => {
+                println!("whoami: option `{}` is unknown", argument);
+                return;
+            }
+        }
+    }
+
+    println!("{}", whoami::GetUsername());
 }
