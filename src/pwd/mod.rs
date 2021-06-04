f504aae55efa9473fc844663e72d54d0d60790a1
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-06-04 15:22:27 +0800

    Update mod.rs

--- src/pwd/mod.rs
+++ src/pwd/mod.rs
@@ -1,1 +1,14 @@
+pub const PWD_HELP_MESSAGE: &str = r"Usage: pwd [OPTION]...
+Prints the current working directory.
 
+Options:
+    --help          prints this message and exit
+    --version       displays version information and exit
+
+Source code for this command: <https://github.com/LinuxCommandsForWindows/LinuxCommandsOnWindows/tree/main/src/pwd>";
+
+pub const PWD_VERSION_MESSAGE: &str = r"pwd (Linux Commands for Windows) 0.1.1
+Copyright (c) 2021 Linux Commands for Windows Project Developers
+License Apache v2.0: <https://www.apache.org/licenses/LICENSE-2.0.txt>
+This is free software: you are free to modify and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.";
