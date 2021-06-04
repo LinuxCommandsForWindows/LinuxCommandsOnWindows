c5ffbd86df2905ab9d2e00c9d8808cf791339110
HTG-YT <39023054+HTG-YT@users.noreply.github.com>
2021-06-04 15:17:44 +0800

    Add files via upload

---
+++ src/pwd/bin/main.rs
@@ -0,0 +1,20 @@
+use std::env;
+
+fn main() {
+    let mut args = env::args();
+
+    if let Some(arg) = args.nth(1) {
+        match &*arg {
+            "--help" => {
+                println!();
+                return;
+            },
+            "--version" => {
+                println!();
+                return;
+            }
+        }
+    }
+
+    println!("/mnt/{}", env::current_dir().unwrap().to_str().unwrap().replace(":", "").replace(r"\", "/").to_ascii_lowercase());
+}
